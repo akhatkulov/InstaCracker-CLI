8b8db6df37d7de4b496ccc5158c5220092a4e8e3
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2023-12-18 21:55:35 +0500

    update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import typer
+from typing import Optional
 import os
 import sys
 import inquirer
 import colorama 
 
-def main(n:str):
-    typer.echo("hello world")
+###################
+from parts import *
+
+def main(status:str = typer.Optional('home',help="Status"),username: str = typer.Optional('none'),passwd_w: str: typer.Optional('-1')):
+    typer.echo("hello world"):
+      if status == "home":
+        home_page()
+      elif status == "start" and passwd_w == "-6w":
+        

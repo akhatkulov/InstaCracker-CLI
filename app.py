4be903ce26f507efd097be7025af5aac426b42e0
akhatkulov <amsmetafra@gmail.com>
2023-12-19 13:41:14 +0500

    update5

--- app.py
+++ app.py
@@ -11,10 +11,119 @@ from helper import *
 
 
 def main(status: Optional[str] = typer.Argument("home"), target: Optional[str] = typer.Argument("none")):
-    home_page()
-    # username = typer.prompt(typer.style("--[x]-- Enter a username",fg=typer.colors.GREEN))
     if status == "home":
       home_page()
+      Lang = [
+        inquirer.List(
+          "language",
+           message="Choose language",
+           choices=[typer.style('Uzbek',fg=typer.colors.GREEN),typer.style("Russian",fg=typer.colors.GREEN),typer.style("English",typer.colors.GREEN)]
+        ),
+      ]
+      x = inquirer.prompt(Lang)
+      if "Uzbek" in x["language"]:
+        mode = [
+          inquirer.List(
+            "mode",
+            message="Parol uzunligini tanlang",
+            choices=[
+              typer.style("odatiy",fg=typer.colors.GREEN),
+              typer.style("6",fg=typer.colors.GREEN),
+              typer.style("7",fg=typer.colors.GREEN),
+              typer.style("8",fg=typer.colors.GREEN),
+              typer.style("9",fg=typer.colors.GREEN),
+              typer.style("10",fg=typer.colors.GREEN),
+              typer.style("11",fg=typer.colors.GREEN),
+              typer.style("12",fg=typer.colors.GREEN),
+              typer.style("13",fg=typer.colors.GREEN),
+              typer.style("14",fg=typer.colors.GREEN),
+              
+            ]
+          ),
+        ]
+        y = inquirer.prompt(mode)
+        if "odatiy" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          default_start(user)
+        elif "6" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w6_start(user)
+        elif "7" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w7_start(user)
+        elif "8" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w8_start(user)
+        elif "9" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w9_start(user)
+        elif "10" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w10_start(user)
+        elif "11" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w11_start(user)
+        elif "12" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w12_start(user)
+        elif "13" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w13_start(user)
+        elif "14" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w14_start(user)
+      ######################## Russian ##################################
+      if "Russian" in x["language"]:
+        mode = [
+          inquirer.List(
+            "mode",
+            message="Parol uzunligini tanlang",
+            choices=[
+              typer.style("odatiy",fg=typer.colors.GREEN),
+              typer.style("6",fg=typer.colors.GREEN),
+              typer.style("7",fg=typer.colors.GREEN),
+              typer.style("8",fg=typer.colors.GREEN),
+              typer.style("9",fg=typer.colors.GREEN),
+              typer.style("10",fg=typer.colors.GREEN),
+              typer.style("11",fg=typer.colors.GREEN),
+              typer.style("12",fg=typer.colors.GREEN),
+              typer.style("13",fg=typer.colors.GREEN),
+              typer.style("14",fg=typer.colors.GREEN),
+              
+            ]
+          ),
+        ]
+        y = inquirer.prompt(mode)
+        if "odatiy" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          default_start(user)
+        elif "6" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w6_start(user)
+        elif "7" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w7_start(user)
+        elif "8" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w8_start(user)
+        elif "9" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w9_start(user)
+        elif "10" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w10_start(user)
+        elif "11" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w11_start(user)
+        elif "12" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w12_start(user)
+        elif "13" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w13_start(user)
+        elif "14" in y["mode"]:
+          user = typer.prompt(f"{typer.style('Foydalanuvchi nomini kiriting',fg=typer.colors.GREEN)}")
+          w14_start(user)
     elif status == "hack--6w":
       w6_start(target)
     elif status == "hack--7w":
